178c38a15beb82ff354a0c3b4319588762c406a5
Davi Bittencourt de Almeida <dbittencourt2002@gmail.com>
2023-09-20 11:31:38 -0300

    feat: classe controladora da tela inicial

---
+++ src/controllers/TelaInicialController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TelaInicialController {
+
+    @FXML
+    private Button botaoEnemAntigo;
+
+    @FXML
+    private Button botaoEnemNovo;
+
+    @FXML
+    void analisarEnemAntigo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void analisarEnemNovo(ActionEvent event) {
+
+    }
+
+}
