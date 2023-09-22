94877a947a3dcb97f6c628ff054a6b03154a83eb
Davi Bittencourt de Almeida <dbittencourt2002@gmail.com>
2023-09-22 16:11:57 -0300

    feat: implementação do controller da tela inicial

--- src/controllers/TelaInicialController.java
+++ src/controllers/TelaInicialController.java
@@ -1,10 +1,12 @@
 package controllers;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class TelaInicialController {
+public class TelaInicialController extends ControllerBase {
 
     @FXML
     private Button botaoEnemAntigo;
@@ -13,13 +15,15 @@ public class TelaInicialController {
     private Button botaoEnemNovo;
 
     @FXML
-    void analisarEnemAntigo(ActionEvent event) {
-
+    void analisarEnemAntigo(ActionEvent event) throws IOException {
+        localizacaoArquivo = "../views/enem_antigo/";
+        setStage(loadFXML(localizacaoArquivo, "tela_inicial"), event);
     }
 
     @FXML
-    void analisarEnemNovo(ActionEvent event) {
-
+    void analisarEnemNovo(ActionEvent event) throws IOException {
+        localizacaoArquivo = "../views/enem_novo/";
+        setStage(loadFXML(localizacaoArquivo, "tela_inicial"), event);
     }
 
 }
