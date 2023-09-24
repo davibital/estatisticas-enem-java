e652875217eea49fd6f3fa7417a46aa54eb099c2
Davi Bittencourt de Almeida <dbittencourt2002@gmail.com>
2023-09-24 11:58:50 -0300

    refactor: mudança da localização das telas de gráficos

--- src/controllers/TelaInicialController.java
+++ src/controllers/TelaInicialController.java
@@ -1,12 +1,15 @@
 package controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-public class TelaInicialController extends ControllerBase {
+public class TelaInicialController extends ControllerBase implements Initializable {
 
   @FXML
   private Button botaoFaixaEtaria;
@@ -31,43 +34,41 @@ public class TelaInicialController extends ControllerBase {
 
   @FXML
   void analisarPresenca(ActionEvent event) throws IOException {
-    localizacaoArquivo = "../views/";
     setStage(loadFXML(localizacaoArquivo, "presenca"), event);
   }
 
   @FXML
   void analisarFaixaEtaria(ActionEvent event) throws IOException {
-    localizacaoArquivo = "../views/";
     setStage(loadFXML(localizacaoArquivo, "faixa_etaria"), event);
   }
 
   @FXML
   void analisarGeneroInscritos(ActionEvent event) throws IOException {
-    localizacaoArquivo = "../views/";
     setStage(loadFXML(localizacaoArquivo, "genero"), event);
   }
 
   @FXML
   void analisarInscritosUF(ActionEvent event) throws IOException {
-    localizacaoArquivo = "../views/";
     setStage(loadFXML(localizacaoArquivo, "uf_inscritos"), event);
   }
 
   @FXML
   void analisarMediaProvaObjetiva(ActionEvent event) throws IOException {
-    localizacaoArquivo = "../views/";
     setStage(loadFXML(localizacaoArquivo, "media_objetiva"), event);
   }
 
   @FXML
   void analisarMediaRedacao(ActionEvent event) throws IOException {
-    localizacaoArquivo = "../views/";
     setStage(loadFXML(localizacaoArquivo, "media_redacao"), event);
   }
 
   @FXML
   void analisarTotalInscritos(ActionEvent event) throws IOException {
-    localizacaoArquivo = "../views/";
     setStage(loadFXML(localizacaoArquivo, "total_inscritos"), event);
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    localizacaoArquivo = "../views/graficos/";
   }
 }
